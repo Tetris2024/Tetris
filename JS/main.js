f7fcb8745cd8ba8e2ea1dc3e0709440ef8df6a56
LaCho7 <59169832+LaCho7@users.noreply.github.com>
2024-06-26 11:04:18 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,11 @@
 document.getElementById('goToSecondPage').addEventListener('click', function () {
     window.location = "../pages/second_page.html";
 });
+
+document.getElementById('goToInscription').addEventListener('click', function () {
+    window.location = "../pages/inscription.html";
+});
+
+document.getElementById('goToConnexion').addEventListener('click', function () {
+    window.location = "../pages/connexion.html";
+});
